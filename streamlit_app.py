dbe424763237d37fd9d47a09c1f49d9e0acd0223
fabrizio-streamlit <fabrizioclerici@gmail.com>
2024-10-03 16:05:22 +0000

    First release

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+from io import StringIO
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.logo(
+    image="https://i.ytimg.com/vi/8UtWJ5CVubg/maxresdefault.jpg",
 )
+fl = st.file_uploader(label="Carica Immagine")
+caratteristiche = st.text_area(label='Caratteristiche',placeholder='Inserisci testo')
+
+
+if fl is not None:
+    # To read file as bytes:
+
+    bytes_data = fl.getvalue()
+    st.write("filename:", fl.name)
+    st.image(bytes_data)
+
+
+
