03047f7612a6edd5d50a177c78c85106c879fd3f
fabrizio-streamlit <fabrizioclerici@gmail.com>
2024-10-03 16:26:02 +0000

    ADD: Button

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,19 @@ from io import StringIO
 
 st.logo(
     image="https://i.ytimg.com/vi/8UtWJ5CVubg/maxresdefault.jpg",
+    size="large"
 )
 fl = st.file_uploader(label="Carica Immagine")
 caratteristiche = st.text_area(label='Caratteristiche',placeholder='Inserisci testo')
 
+if st.button(label="Crea Descrizione"):
+    if (caratteristiche != "") and (fl is not None):
+        st.write('Elaborazione completata')
+    else:
+        if fl is None:
+            st.toast('Immagine non inserita')
+        else:
+            st.toast('Caratteristiche non inserite')
 
 if fl is not None:
     # To read file as bytes:
